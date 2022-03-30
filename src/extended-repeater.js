2016a92fc691c413578dde85aed5aa72060e7d8f
Philipp Khromov <filooloog@mail.ru>
2022-03-30 21:15:18 +0300

    feat: done 'extended-repeater.js'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  let additionString = '';
+  if (options.hasOwnProperty('addition')) {
+    let additionArray = new Array(options.additionRepeatTimes || 1);
+    options.addition = String(options.addition);
+    additionArray.fill(options.addition, 0, options.additionRepeatTimes || 1)
+    additionString = additionArray.join(options.additionSeparator || '|');
+    str += additionString;
+  }
+
+  let stringArray = new Array(options.repeatTimes || 1);
+  stringArray.fill(str, 0, options.repeatTimes || 1)
+
+  return stringArray.join(options.separator || '+');
 }
 
 module.exports = {
