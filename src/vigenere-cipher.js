e887985de02f2f1f347596a857673703e2ec8de0
Philipp Khromov <filooloog@mail.ru>
2022-04-06 22:35:30 +0300

    feat: done 'vigenere-cipher.js'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(condition) {
+    if (condition === undefined) {
+      this.condition = true;
+    } else {
+      this.condition = condition;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if(!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const keyLength = key.length;
+    const alphabetPower = 26;
+    const firstCharCode = 65;
+    let charIndex = 0;
+    const encryptedString = [...message].map(char => {
+      const charCode = char.toUpperCase().charCodeAt(0) - firstCharCode;
+      if (charCode >= 0 && charCode <= 26) {
+        const keyCharPosition = charIndex++ % keyLength;
+        const keyChar = key[keyCharPosition];
+        const keyCharCode = keyChar.toUpperCase().charCodeAt(0) - firstCharCode;
+        const newCharCode = (charCode + keyCharCode) % alphabetPower;
+        return String.fromCharCode(newCharCode + firstCharCode)
+      } else {
+        return char
+      }
+    }).join('');
+
+    if (this.condition) {
+      // straight
+      return encryptedString
+    } else {
+      // reverse
+      return encryptedString.split('').reverse().join('')
+    }
+  }
+
+  decrypt(message, key) {
+    if(!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const keyLength = key.length;
+    const alphabetPower = 26;
+    const firstCharCode = 65;
+    let charIndex = 0;
+    const decryptedString = [...message].map(char => {
+      const charCode = char.toUpperCase().charCodeAt(0) - firstCharCode;
+      if (charCode >= 0 && charCode <= 26) {
+        const keyCharPosition = charIndex++ % keyLength;
+        const keyChar = key[keyCharPosition];
+        const keyCharCode = keyChar.toUpperCase().charCodeAt(0) - firstCharCode;
+        let newCharCode = (charCode - keyCharCode) % alphabetPower;
+        if (newCharCode < 0) {
+          newCharCode = alphabetPower - (-newCharCode);
+        }
+        return String.fromCharCode(newCharCode + firstCharCode)
+      } else {
+        return char
+      }
+    }).join('');
+    
+    if (this.condition) {
+      // straight
+      return decryptedString
+    } else {
+      // reverse
+      return decryptedString.split('').reverse().join('')
+    }
   }
 }
 
